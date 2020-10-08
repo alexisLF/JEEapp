6c6a57e161135102fde22c94bc5fd18265b2f24b
alexisLF <tel0231724347>
2020-10-08 16:59:15 +0200

    add relation

--- src/main/java/com/example/appjee/entities/Employee.java
+++ src/main/java/com/example/appjee/entities/Employee.java
@@ -1,6 +1,7 @@
 package com.example.appjee.entities;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "employees")
@@ -13,8 +14,35 @@ public class Employee {
     @Column(name = "last_name")
     private String lastname;
 
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "employee_function",
+            joinColumns = @JoinColumn(name = "employee_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "function_id",
+                    referencedColumnName = "id"))
+    private List<Function> functions;
+
+    @ManyToOne
+    @JoinColumn(name="role_id", nullable=true)
+    private Role role;
+
     public Employee(){
 
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<Function> getFunctions() {
+        return functions;
+    }
+
+    public void setFunctions(List<Function> functions) {
+        this.functions = functions;
     }
 
     public Long getId() {
@@ -39,5 +67,15 @@ public class Employee {
 
     public void setLastname(String lastname) {
         this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", functions=" + functions +
+                '}';
     }
 }
